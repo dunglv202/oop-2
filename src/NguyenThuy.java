c1cad767d62e1715b68fa6c50964500d0fcba39f
Luu Van Dung <admin@dunglv202.onmicrosoft.com>
2022-10-30 15:24:51 +0700

    sfa

--- src/NguyenThuy.java
+++ src/NguyenThuy.java
@@ -5,48 +5,26 @@ public class NguyenThuy {
     Scanner scanner = new Scanner(System.in);
 
     int t = scanner.nextInt();
-
-    // init
-    List<String> l = solve(10000);
-
     while (t-- > 0) {
-      int n = scanner.nextInt();
-      Iterator<String> iterator = l.stream().iterator();
-      for (int i=0; i<n; i++) {
-        System.out.print(iterator.next() + " ");
-      }
+      solve(scanner.nextInt());
+      System.out.println();
     }
   }
 
-  private static List<String> solve(int n) {
-    List<String> result = new LinkedList<>();
+  private static void solve(int n) {
     Queue<String> q = new LinkedList<>();
 
-    q.add("");
+    q.add("4");
+    q.add("5");
     int found = 0;
     while (found < n) {
-      String cur = q.peek();
+      String s = q.peek();
+      System.out.print(s + new StringBuilder(s).reverse() + " ");
+      found++;
 
-      if (check(cur)) {
-        result.add(cur);
-        found++;
-      }
-
-      q.add(cur + "4");
-      q.add(cur + "5");
-
+      q.add(s + "4");
+      q.add(s + "5");
       q.poll();
     }
-
-    return result;
-  }
-
-  private static boolean check(String n) {
-    return n.length()%2 == 0 && isSymmetric(n);
-  }
-
-  private static boolean isSymmetric(String s) {
-    String rs = new StringBuilder(s).reverse().toString();
-    return rs.equals(s);
   }
 }
